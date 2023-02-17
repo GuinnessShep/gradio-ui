0bd4f0d73161746873b4cd2854a2e182657d31bc
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-17 10:10:23 -0800

    Update ailauncher.py

--- ailauncher.py
+++ ailauncher.py
@@ -1,4 +1,4 @@
-Model = "Pygmalion 6B Experimental" #@param ["Pygmalion 350M", "Pygmalion 1.3B", "Pygmalion 2.7B", "Pygmalion 6B", "Pygmalion 6B Experimental"] {allow-input: true}
+Model = "Pygmalion 6B Experimental"
 
 pretty_model_name_to_hf_name = {
     "Pygmalion 350M": "PygmalionAI/pygmalion-350m",
@@ -12,24 +12,19 @@ model_name = pretty_model_name_to_hf_name[Model]
 branch_name = "main" if Model != "Pygmalion 6B Experimental" else "dev" 
 
 import os
-if not os.path.exists("/content/drive"):
-  os.mkdir("/content/drive")
-if not os.path.exists("/content/drive/MyDrive/"):
-  os.mkdir("/content/drive/MyDrive/")
+import subprocess
 
-!wget https://koboldai.org/ckds -O - | bash /dev/stdin --init only
+subprocess.run(["wget", "https://koboldai.org/ckds", "-O", "-", "|", "bash", "/dev/stdin", "--init", "only"])
 
-!git clone --depth=1 \
-  "https://github.com/PygmalionAI/gradio-ui.git" \
-  && cd gradio-ui && pip3 install -r requirements.txt
+subprocess.run(["git", "clone", "--depth=1", "https://github.com/PygmalionAI/gradio-ui.git"])
+subprocess.run(["cd", "gradio-ui", "&&", "pip3", "install", "-r", "requirements.txt"])
 
 print("\n\n\n")
-print("* The model is about to be downloaded and loaded into the GPU.")
+print("* The model is about to be downloaded and loaded.")
 print("* This takes several minutes, sit tight.")
 print("* A link will show up when this step is completed, keep checking back every couple minutes or so.")
 print("\n\n\n")
-os.system(f"cd /content/KoboldAI-Client && python3 aiserver.py --noaimenu --host --port 80808 --model {model_name} --revision {branch_name} --nobreakmodel --lowmem --quiet &")
 
-!python3 gradio-ui/src/app.py \
-  --koboldai-url "http://localhost:80808" \
-  --share
+subprocess.Popen(f"python3 /content/KoboldAI-Client/aiserver.py --noaimenu --host --port 80808 --model {model_name} --revision {branch_name} --nobreakmodel --lowmem --quiet", shell=True)
+
+subprocess.run(["python3", "gradio-ui/src/app.py", "--koboldai-url", "http://localhost:80808", "--share"])
